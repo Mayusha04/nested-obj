2f4c60a2ab31c42bdc923166bc13892c8ea0d9a3
Mayusha04 <mayushachowdary95@gmail.com>
2021-12-20 05:27:31 +0530

    nested

---
+++ nested.js
@@ -0,0 +1,26 @@
+var student ={
+    name:"vinay",
+    Age:17,
+    marks:{
+        science:68,
+        maths:75,
+        physics:73,
+    }
+}
+console.log(student.name)
+console.log(student.Age)
+console.log(student.marks)
+console.log(typeof student)
+
+var car ={
+    model:"Swift",
+    owner:"xyz",
+    features:{
+        mileage:20,
+        fuel:"petrol",
+        airbags:"2"
+    }
+}
+console.log(car.model);
+console.log(car.features.mileage);
+console.log(car.features);
